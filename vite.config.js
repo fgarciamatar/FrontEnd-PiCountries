28554d387aeac3e25807419fd9f86c1a9ebf0018
fgarciamatar <fgarciamatar@gmail.com>
2023-09-14 22:33:09 -0300

    deplo

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+import { defineConfig } from 'vite';
+
 export default defineConfig({
-  plugins: [react()],
-})
+  // ...
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://backend-countriespi-production.up.railway.app/', // Cambia esto por la URL de tu servidor en Railway
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+});
